54dea72d5facbd0f7c7a7f5d1d1471738e79ea62
AliGh1 <ali.agk25@gmail.com>
2024-08-23 20:05:47 +0330

    feat(auth): add forgot-password functionality

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\NewPasswordController;
+use App\Http\Controllers\Api\V1\PasswordResetLinkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +14,14 @@ Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])
         ->middleware('guest.api')
         ->name('login');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest.api')
+        ->name('password.email');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest.api')
+        ->name('password.store');
 
     Route::post('/logout', [AuthController::class, 'logout'])
         ->middleware('auth:sanctum')
